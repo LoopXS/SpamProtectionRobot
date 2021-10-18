342f59554b3e3c304429d31e2457976efc6e1d68
LoopXS <91501744+LoopXS@users.noreply.github.com>
2021-10-18 08:33:08 +0330

    Update __main__.py

--- spr/__main__.py
+++ spr/__main__.py
@@ -49,9 +49,8 @@ async def help_command(_, message: Message):
     kb = ikb(
         {
             "Help": "bot_commands",
-            "Repo": "https://github.com/TheHamkerCat/SpamProtectionRobot",
             "Add Me To Your Group": f"https://t.me/{BOT_USERNAME}?startgroup=new",
-            "Support Chat (for now)": "https://t.me/WBBSupport",
+            "Support": "https://t.me/DarkPentester",
         }
     )
     mention = message.from_user.mention
@@ -83,7 +82,7 @@ async def help_parser(name, keyboard=None):
             paginate_modules(0, HELPABLE, "help")
         )
     return (
-        f"Hello {name}, I'm SpamProtectionRobot, I can protect "
+        f"Hello {name}, I'm SpamProtectionBot, I can protect "
         + "your group from Spam and NSFW media using "
         + "machine learning. Choose an option from below.",
         keyboard,
